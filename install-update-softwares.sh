4ab2cf81dc56d91dfbf22c35f502b2f79a67e25d
edxlinux-plus <antoniabarbosa2608@gmail.com>
2025-07-27 19:47:18 -0300

    Add files via upload

---
+++ install-update-softwares.sh
@@ -0,0 +1,19 @@
+                          ./install-update-softwares.sh
+
+
+apt update -y
+
+
+apt install icewm -y
+
+
+apt install qbittorrent -y
+
+
+apt install gparted -y
+
+apt install clementine -y
+
+
+
+
